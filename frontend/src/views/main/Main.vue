6e486e6b304a0e598ac64a7054e925f4b0cf5ac6
gujerbit <ghftm1124@gmail.com>
2022-08-22 16:10:42 +0900

    add tailwindcss

---
+++ frontend/src/views/main/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="justify-center flex bg-yellow-300 items-center h-screen">
+        <div class="text-4xl">
+            Hello
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Main",
+});
+</script>
+
+<style>
+
+</style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,4 +3,14 @@ const path = require("path");
 module.exports = {
     outputDir: path.resolve(__dirname, "../backend/public"),
     assetsDir: "./assets",
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+                "@assets": path.resolve(__dirname, "src/assets"),
+                "@components": path.resolve(__dirname, "src/components"),
+                "@views": path.resolve(__dirname, "src/views"),
+            }
+        }
+    }
 };
