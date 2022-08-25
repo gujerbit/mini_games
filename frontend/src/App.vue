5878194b07c0737c4ffed073ba28003c658b4542
gujerbit <ghftm1124@gmail.com>
2022-08-25 15:36:13 +0900

    footer 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,14 @@
 <template>
-    <router-view></router-view>
+    <div id="container">
+        <router-view></router-view>
+
+        <footer class="w-full h-8 flex justify-center items-center absolute bottom-0 border-t border-black bg-white">
+            <p class="mr-2">&copy; 2022-08-25. jylee. All rights reserved.</p>
+            <a href="https://github.com/gujerbit" target="_blank">
+                <img  src="@assets/images/github.png" alt="">
+            </a>
+        </footer>
+    </div>
 </template>
 
 <style>
