a8cf2d1862230a1c53b1c1d0def401918e7151b3
gujerbit <ghftm1124@gmail.com>
2022-09-05 18:02:00 +0900

    Vector 구현 중

--- frontend/src/utils/Vector.ts
+++ frontend/src/utils/Vector.ts
@@ -18,4 +18,40 @@ export default class Vector {
         return this;
     }
 
+    sub (vector:Vector) {
+        this.x -= vector.x;
+        this.y -= vector.y;
+        this.z -= vector.z;
+
+        return this;
+    }
+
+    mul (n:number) {
+        this.x *= n;
+        this.y *= n;
+        this.z *= n;
+
+        return this;
+    }
+
+    div (n:number) {
+        this.x /= n;
+        this.y /= n;
+        this.z /= n;
+
+        return this;
+    }
+
+    mag () {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+
+    normalize () {
+        if (this.mag() === 0) {
+            return;
+        }
+
+        this.div(this.mag());
+    }
+
 }
